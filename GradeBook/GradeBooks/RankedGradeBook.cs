afa7daec6b1ea642aec1ac16011793c70165425e
Konrad Lempart <Konrad.Lempart@microsoft.wsei.edu.pl>
2022-03-27 22:26:32 +0200

    20/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name, bool isWeight) : base(name, isWeight)
         {
